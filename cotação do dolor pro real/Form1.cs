154ae99ae5ed4bdf3a6098031fb5e39288fc1227
alexandro.r.c.espanhol <112526166+alexandroespanhol@users.noreply.github.com>
2023-04-25 22:06:16 -0300

    Adicionar arquivos de projeto.

---
+++ cotação do dolor pro real/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cotação_do_dolor_pro_real
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double valor1, valor2;
+            valor1 = Convert.ToDouble(textBox1.Text);
+            valor2 = Convert.ToDouble(textBox2.Text);
+
+            label3.Text = Convert.ToString(valor1 * valor2, CultureInfo.InvariantCulture);
+                }
+    }
+}
